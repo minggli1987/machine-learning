170fa9d4b2d20a203363acc457ea99e372f66df4
Ming <ming.george.li@gmail.com>
2016-08-17 18:08:20 +0100

    new lib

---
+++ regression lib.py
@@ -0,0 +1,58 @@
+import statsmodels.api as sm
+
+__author__ = 'Ming Li @ London, UK'
+
+
+def forward_selected(data, target):
+    """Linear model designed by forward selection.
+
+    Parameters:
+    -----------
+    data : pandas DataFrame with all possible predictors and response w
+
+    response: string, name of response column in data
+
+    Returns:
+    --------
+    model: an "optimal" fitted statsmodels linear model
+           with an intercept
+           selected by forward selection
+           evaluated by adjusted R-squared
+    """
+
+    remaining = set(data.columns)
+    remaining.remove(target)
+    selected_var = []
+    current_score, best_new_score = 0.0, 0.0
+    alpha = 0.05
+
+    print('\nbeginning forward stepwise variable selection...\n')
+    while remaining and current_score == best_new_score:
+
+        scores_with_candidates = []  # containing variables
+
+        for candidate in remaining:
+
+            X = sm.add_constant(data[selected_var + [candidate]])  # inherit variables from last step and try new ones
+            reg = sm.OLS(data[target], X).fit()
+            score, p = reg.rsquared, reg.pvalues[-1]  # r2 (changeable) and two-tailed p value of the candidate
+            scores_with_candidates.append((score, p, candidate))
+
+        scores_with_candidates.sort(reverse=False)  # order variables by score in ascending
+        disqualified_candidates = [i for i in scores_with_candidates if ~(i[1] < alpha)]
+        scores_with_candidates = [i for i in scores_with_candidates if i[1] < alpha]
+        try:
+            best_new_score, best_candidate_p, best_candidate = scores_with_candidates.pop()
+        except IndexError:  # when no candidate passes significance test at critical value
+            disqualified_score, disqualified_p, disqualified_candidate = disqualified_candidates.pop(0)
+            remaining.remove(disqualified_candidate)  # remove the worst disqualified candidate
+#            print(disqualified_score, disqualified_p, disqualified_candidate)
+            continue  # continuing the while loop
+        if current_score < best_new_score:
+            remaining.remove(best_candidate)
+            current_score = best_new_score
+            selected_var.append(best_candidate)
+#            print(best_new_score, best_candidate_p, best_candidate)
+
+    model = sm.OLS(data[target], sm.add_constant(data[selected_var])).fit()
+    return model, selected_var
