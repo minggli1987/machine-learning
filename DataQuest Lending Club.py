c66fe50d6dbbcb355e58ce5b226f4ecdb13e1851
Ming <ming.george.li@gmail.com>
2016-08-22 14:50:36 +0100

    new lib

--- DataQuest Lending Club.py
+++ DataQuest Lending Club.py
@@ -13,9 +13,6 @@ if not os.path.exists('data/loans_2007.csv'):
 
 loans_2007 = pd.read_csv('data/loans_2007.csv', encoding='ISO-8859-1', low_memory=False)
 
-print(loans_2007.dtypes)
-print(loans_2007.head())
-
 drop = ['id', 'member_id', 'funded_amnt', 'funded_amnt_inv', 'grade', 'sub_grade', 'emp_title', 'issue_d']
 
 loans_2007.drop(drop, inplace=True, axis=1)
@@ -25,11 +22,20 @@ loans_2007.drop(drop2, inplace=True, axis=1)
 
 drop3 = ['total_rec_int', 'total_rec_late_fee', 'recoveries', 'collection_recovery_fee', 'last_pymnt_d', 'last_pymnt_amnt']
 loans_2007.drop(drop3, inplace=True, axis=1)
-print(loans_2007.dtypes)
 
-print(loans_2007['loan_status'].value_counts())
+# Target
+target = 'loan_status'
+
+# Only keep Fully Paid and Charged Off and ignore other statues
+
+drop4 = loans_2007[target].value_counts()[:2].index
+loans_2007 = loans_2007[loans_2007[target].isin(drop4)]
+
+loans_2007[target] = loans_2007[target].astype('category').cat.codes
 
 
-def categorical_to_code(var):
-    return pd.Categorical.from_array(var).codes
+data_mapping = dict()
+for i in loans_2007.columns:
+    data_mapping[i] = loans_2007[i].unique().shape[0]
 
+print({k: v for k, v in data_mapping.items() if v < 2})
