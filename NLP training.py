8d19a9c7ed21a476f697c4e43c160886fa098043
Ming <ming.george.li@gmail.com>
2016-08-31 10:40:35 +0100

    new lib

--- NLP training.py
+++ NLP training.py
@@ -1,6 +1,8 @@
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews as mr
+from nltk.tokenize import word_tokenize
+import string
 
 
 def word_feats(words):
@@ -17,9 +19,29 @@ poscutoff = int(len(posfeats) * 3 / 4)
 
 trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
 testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
-print ('train on {:.2f} instances, test on {:.2f} instances'.format(len(trainfeats), len(testfeats)))
+
+print('train on {:.2f} instances, test on {:.2f} instances'.format(len(trainfeats), len(testfeats)))
 
 classifier = NaiveBayesClassifier.train(trainfeats)
-print ('accuracy:', nltk.classify.util.accuracy(classifier, testfeats))
+print('accuracy:', nltk.classify.util.accuracy(classifier, testfeats))
 
 classifier.show_most_informative_features()
+
+bad_review = 'As ever, Paolo Sorrentino ironically cuts the legs out from under his protagonists\' wistfulness with grotesquerie'
+good_review = '''Paolo Sorrentino, with Youth, delivers his most tender film to date, an emotionally rich contemplation of life's wisdom gained, lost and remembered - with cynicism harping from the sidelines, but as a wearied chord rather than a major motif.'''
+
+
+def classify(review):
+
+    assert isinstance(review, str), 'review must be string'
+
+    # tokenize the review in lower case without punctuation
+
+    cleansed_review = review.lower().translate(str.maketrans('', '', string.punctuation))
+    tokenized_review = word_tokenize(cleansed_review, language='english')
+    feats = dict([(word, True) for word in tokenized_review])
+    print('\n', review, '\n', 'NLP classifer believes above review is:')
+    return classifier.classify(feats)
+
+
+print(classify(34534))
