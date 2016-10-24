b93110d90c57841586715e088ce6ce30d27fa2ab
Ming <ming.george.li@gmail.com>
2016-10-24 15:31:13 +0100

    new commit

--- leaf classification/image_feature_extraction.py
+++ leaf classification/image_feature_extraction.py
@@ -6,7 +6,7 @@ import matplotlib.image as mpimg
 from skimage import measure, io, color
 import scipy.ndimage as ndi
 from skimage.feature import corner_harris, corner_subpix, corner_peaks, CENSURE
-
+from scipy.signal import argrelextrema
 
 __author__ = 'Ming Li'
 # This application forms a submission from Ming in regards to leaf classification challenge on Kaggle community
@@ -14,7 +14,10 @@ __author__ = 'Ming Li'
 path = 'leaf/images/'
 pic_names = {i: path + str(i) + '.jpg' for i in range(1, 1585, 1)}
 
-pic = pic_names[1000]
+
+# exploring possible feature
+
+pic = pic_names[99]
 
 print(pic)
 
@@ -31,13 +34,29 @@ contour = max(contours, key=len)
 
 # demean contour coordinates
 
-contour[:, 0] -= cy
-contour[:, 1] -= cx
+polar_contour = contour.copy()
+polar_contour[:, 0] -= cy
+polar_contour[:, 1] -= cx
 
 
 # let us see the contour that we hopefully found
-plt.plot(contour[::, 1], contour[::, 0], linewidth=1, color='black')  # (I will explain this [::,x] later)
+plt.plot(polar_contour[::, 1], polar_contour[::, 0], linewidth=1, color='black')  # (I will explain this [::,x] later)
 plt.scatter(0, 0)
 plt.title(pic)
 plt.clf()
+
+# for local maxima
+c_max_index = argrelextrema(polar_contour[:, 0], np.greater, order=50)
+c_min_index = argrelextrema(polar_contour[:, 0], np.less, order=50)
+
+plt.scatter(polar_contour[:, 1], polar_contour[:, 0],
+            linewidth=0, s=2, c='k')
+plt.scatter(polar_contour[:, 1][c_max_index],
+            polar_contour[:, 0][c_max_index],
+            linewidth=0, s=30, c='b')
+plt.scatter(polar_contour[:, 1][c_min_index],
+            polar_contour[:, 0][c_min_index],
+            linewidth=0, s=30, c='r')
+
+
 plt.show()
