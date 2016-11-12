3fe8be48235f276acd9cb5ee36b02c0cf2059625
Ming <ming.george.li@gmail.com>
2016-11-12 18:08:15 +0000

    new commit

--- NLP/chatbot/text_mining.py
+++ NLP/chatbot/text_mining.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
+import pandas as pd
 
 web_pages = {
     1: 'http://www.nhs.uk/Conditions/Heart-block/Pages/Symptoms.aspx',
@@ -18,35 +19,52 @@ web_pages = {
     'Pages/Symptoms.aspx'
 }
 
+#
+# for i in range(1, len(web_pages) + 1, 1):
+#
+#     m = re.search('conditions/(.*)/pages/', web_pages[i].lower()).group(1)
+#     m = re.sub('[^0-9a-zA-Z]+', ' ', m)
+#     web_pages[m] = web_pages.pop(i)
+#
+# illness = list(web_pages.keys())
+# illness.sort(reverse=True)
+# print(illness)
 
-for i in range(1, len(web_pages) + 1, 1):
-
-    m = re.search('conditions/(.*)/pages/', web_pages[i].lower()).group(1)
-    m = re.sub('[^0-9a-zA-Z]+', ' ', m)
-    web_pages[m] = web_pages.pop(i)
-
-illness = list(web_pages.keys())
-illness.sort(reverse=True)
-print(illness)
-for i in illness:
-    i = 'heart block'
+for i in list(web_pages.keys()):
     r = requests.get(url=web_pages[i])
-    data = r.text
-    soup = BeautifulSoup(data, 'html5lib')
+    soup = BeautifulSoup(r.text, 'html5lib')
+    # for i in soup.find_all('h1'):
+    #     print(i)
 
 # web page overview
 print('scrapping web page for...{0}.'.format(soup.title.string), '\n\n')
-time.sleep(3)
+time.sleep(2)
 html = soup.prettify()
-print(html)
+# print(html, flush=False)
 
-# for i in soup.find_all(['p', 'li']):
-#     print(i.string)
+desc_attributes = {
+    'name': 'description'
+}
 
+subj_attributes = {
+    'name': 'DC.Subject',
+    'scheme': 'NHSC.Ontology'
 
-# for link in soup.find_all(['li']):
-#     txt = link.string
-#     if txt and len(txt.replace(' ', '')) > 5:
-#         print(txt, end='\n\n')
+}
 
+article_attributes = {
+    'start_t_0': 'Overview',
+    'start_t_1': '\n        Print this page\n    ',
+    'start_t_2': '\n     \n    \n        Print this page\n    \n    \n     \n\n',
+    'end_t_0': 'Share:',
+    'end_t_1': '',
+    'end_t_2': ''
+}
 
+article = list()
+
+for i in soup.find_all(['p', 'li', 'meta']):
+    article.append(i.get_text())
+
+start_t_2 = article.index(article_attributes['start_t_2'])
+print(article[start_t_2:])
