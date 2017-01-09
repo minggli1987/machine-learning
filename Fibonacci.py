91ed0e8c88bd6e24163c20a55af7948b3e7fcf8f
Ming <ming.george.li@gmail.com>
2017-01-09 18:01:16 +0000

    new commit

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,17 +1,29 @@
+import numpy as np
+
+
 def fibonacci(n):
     assert n == int(n) and n > 0
     if n == 1 or n == 2:
         return 1
     return fibonacci(n-1) + fibonacci(n-2)
 
-x =  7 #change the number
+x = 7 #change the number
 
 print(fibonacci(x))
-
 
 
 def power_of_two(x):
     return x == 1 if x < 2 else power_of_two(x/2)
 
 
-print(power_of_two(8))
+def log_power_of_two(x):
+    import numpy as np
+    return x > 0 and np.log2(x) % 1 == 0
+
+test = -1
+
+print(power_of_two(test))
+
+print(log_power_of_two(test))
+
+
