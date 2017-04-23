b8cbff6abc6fcbcbef67bce7150f3996e44a453c
Ming Li <ming.george.li@gmail.com>
2017-04-24 00:26:12 +0100

    update rnn trial1

--- rnn_trial1.py
+++ rnn_trial1.py
@@ -4,6 +4,7 @@
 import random
 
 import tensorflow as tf
+import numpy as np
 
 from sklearn import model_selection
 # for testing purpose, fixing random seed
@@ -25,15 +26,21 @@ def generate_training_data(total_size, seq_length):
     num_seq = -(-total_size // seq_length)
 
     sequence_nested = list()
-    ones_count = list()
+    labels = list()
     for n in range(num_seq):
         index_start = n * seq_length
         index_end = min(total_size, (n + 1) * seq_length)
         sequence_sub = sequence_whole[index_start:index_end]
-        sequence_nested.append(sequence_sub)
-        ones_count.append(sequence_sub.count(1))
+        sequence_nested.append(np.array(sequence_sub))
+        labels.append(sequence_sub.count(1))
 
-    return sequence_nested, ones_count
+    train = np.array(sequence_nested).reshape(-1, 1, seq_length)
+    label = np.array(labels)
+
+    # one-hot label
+    one_hot_label = np.eye(seq_length + 1)[label]
+
+    return train, one_hot_label
 
 
 def weight_variable(shape):
@@ -46,7 +53,7 @@ def bias_variable(shape):
         return tf.Variable(initial)
 
 
-train, label = generate_training_data(200, 20)
+train, label = generate_training_data(int(1e5), 20)
 
 X_train, X_test, y_train, y_test = \
                 model_selection.train_test_split(train,
@@ -54,9 +61,8 @@ X_train, X_test, y_train, y_test = \
                                                  test_size=.2,
                                                  random_state=0)
 
-
-x = tf.placeholder(dtype=tf.uint8, shape=[-1, 20, 1], name='feature')
-y_ = tf.placeholder(dtype=tf.uint8, shape=[-1, 21], name='label')
+x = tf.placeholder(dtype=tf.float32, shape=[None, 1, 20], name='feature')
+y_ = tf.placeholder(dtype=tf.uint8, shape=[None, 21], name='label')
 
 num_hidden = 24
 
@@ -66,38 +72,40 @@ lstm_cell = tf.contrib.rnn.LSTMCell(num_units=num_hidden,
                                     use_peepholes=False)
 
 # unrolled LSTM neural network, dynamnic_rnn or static_rnn
-if True:
-    outputs, final_state = tf.contrib.rnn.static_rnn(cell=lstm_cell, data=x)
 if False:
-    outputs, final_state = tf.nn.dynamic_rnn(cell=lstm_cell, data=x)
+    outputs, final_state = tf.contrib.rnn.static_rnn(cell=lstm_cell,
+                                                     inputs=x,
+                                                     dtype=tf.float32)
+if True:
+    outputs, final_state = tf.nn.dynamic_rnn(cell=lstm_cell,
+                                             inputs=x,
+                                             dtype=tf.float32)
 
-print(outputs.get_shape())
 
 outputs_transposed = tf.transpose(outputs, [1, 0, 2])
 last = tf.gather(outputs_transposed, int(outputs_transposed.get_shape()[0]) - 1)
 
-weight = weight_variable(shape=[num_hidden, y_.get_shape()[1]])
+weight = weight_variable(shape=[num_hidden, int(y_.get_shape()[1])])
 bias = bias_variable(shape=[y_.get_shape()[1]])
 
 logits = tf.matmul(last, weight) + bias
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, y_)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+                                                        labels=y_)
 loss = tf.reduce_mean(cross_entropy)
-train_step = tf.train.RMSPropOptimizer(learning_rate=1e-4).minimize(loss)
+train_step = tf.train.RMSPropOptimizer(learning_rate=1e-3).minimize(loss)
 
 # eval
 mistakes = tf.not_equal(tf.argmax(logits, 1), tf.argmax(y_, 1))
 error_rate = tf.reduce_mean(tf.cast(mistakes, tf.float32))
 
-
-init_op = tf.initialize_all_variables()
+init_op = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init_op)
 
-
 batch_size = 1000
 no_of_batches = int(len(X_train)/batch_size)
-epoch = 5000
+epoch = 1000
 for i in range(epoch):
     ptr = 0
     for j in range(no_of_batches):
