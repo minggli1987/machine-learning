96b10eddf3fc0bcf0f1d47147896527c78859b82
Ming Li <ming.george.li@gmail.com>
2017-07-30 13:45:55 +0100

    workon clustering

--- clustering.py
+++ clustering.py
@@ -2,8 +2,10 @@
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 from sklearn.cluster import AffinityPropagation, DBSCAN, KMeans
+from sklearn.mixture import GaussianMixture
 from sklearn.metrics import v_measure_score, silhouette_score
 from sklearn.datasets import load_boston, load_iris, load_digits
 from sklearn.manifold import TSNE
@@ -16,10 +18,15 @@ iris, iris_target = load_iris()['data'], load_iris()['target']
 db = KMeans(n_clusters=3)
 iris_clusters = db.fit_predict(iris)
 KMeans_iris = v_measure_score(iris_target, iris_clusters)
-print(iris_clusters)
-print(iris_target)
 print(KMeans_iris)
 
+gm = GaussianMixture(n_components=3)
+gm.fit(iris)
+gm_pred = gm.predict(iris)
+gm_iris = v_measure_score(gm_pred, iris_target)
+print(gm_iris)
+print(gm.means_)
+print(gm.covariances_)
 
-coefficientis = silhouette_score(iris, iris_clusters)
-print(coefficientis)
+
+print(np.isfinite(np.nan))
