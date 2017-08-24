b313a47bd7e4847a49fbe356cf359c8e0df6ec25
Ming Li <ming.george.li@gmail.com>
2017-08-24 22:21:56 +0100

    flake8 gp.py

--- gp.py
+++ gp.py
@@ -1,20 +1,24 @@
+"""
+This is code for simple GP regression. It assumes a zero mean GP Prior.
+"""
+
 import numpy as np
 import matplotlib.pyplot as pl
 
-""" This is code for simple GP regression. It assumes a zero mean GP Prior """
-
 
 # This is the true unknown function we are trying to approximate
-f = lambda x: np.sin(0.9*x).flatten()
-#f = lambda x: (0.25*(x**2)).flatten()
+def f(x):
+    return np.sin(0.9*x).flatten()
 
 
+# f = lambda x: (0.25*(x**2)).flatten()
 # Define the kernel
-def kernel(a, b):
+def kernel(a, b, sigma=.1):
     """ GP squared exponential kernel """
-    kernelParameter = 0.1
-    sqdist = np.sum(a**2,1).reshape(-1,1) + np.sum(b**2,1) - 2*np.dot(a, b.T)
-    return np.exp(-.5 * (1/kernelParameter) * sqdist)
+    sqdist = np.sum(a**2, 1).reshape(-1, 1) + np.sum(b**2, 1) - \
+        2*np.dot(a, b.T)
+    return np.exp(-.5 * (1/sigma) * sqdist)
+
 
 N = 10         # number of training points.
 n = 50         # number of test points.
@@ -22,14 +26,14 @@ s = 0.00005    # noise variance.
 
 # Sample some input points and noisy versions of the function evaluated at
 # these points.
-X = np.random.uniform(-5, 5, size=(N,1))
+X = np.random.uniform(-5, 5, size=(N, 1))
 y = f(X) + s*np.random.randn(N)
 
 K = kernel(X, X)
 L = np.linalg.cholesky(K + s*np.eye(N))
 
 # points we're going to make predictions at.
-Xtest = np.linspace(-5, 5, n).reshape(-1,1)
+Xtest = np.linspace(-5, 5, n).reshape(-1, 1)
 
 # compute the mean at our test points.
 Lk = np.linalg.solve(L, kernel(X, Xtest))
@@ -54,7 +58,7 @@ pl.axis([-5, 5, -3, 3])
 
 # draw samples from the prior at our test points.
 L = np.linalg.cholesky(K_ + 1e-6*np.eye(n))
-f_prior = np.dot(L, np.random.normal(size=(n,10)))
+f_prior = np.dot(L, np.random.normal(size=(n, 10)))
 pl.figure(2)
 pl.clf()
 pl.plot(Xtest, f_prior)
@@ -64,7 +68,7 @@ pl.axis([-5, 5, -3, 3])
 
 # draw samples from the posterior at our test points.
 L = np.linalg.cholesky(K_ + 1e-6*np.eye(n) - np.dot(Lk.T, Lk))
-f_post = mu.reshape(-1,1) + np.dot(L, np.random.normal(size=(n,10)))
+f_post = mu.reshape(-1, 1) + np.dot(L, np.random.normal(size=(n, 10)))
 pl.figure(3)
 pl.clf()
 pl.plot(Xtest, f_post)
