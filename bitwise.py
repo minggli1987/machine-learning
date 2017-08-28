1942dae136a7e9be0069a07ca878f43b9277dc30
Ming Li <ming.george.li@gmail.com>
2017-08-28 23:48:47 +0100

    bitwise

--- bitwise.py
+++ bitwise.py
@@ -11,20 +11,32 @@ d = bin(8)
 
 e = bin(16)
 
-for i in range(33):
-	print(str(i), bin(i))
+
+def power_of_two_bitwise(n):
+    """bitwise power of two check."""
+    return n > 0 and n & (n - 1) == 0
 
 
-def power_of_two(n):
-	print(bin(n & (n - 1)))
-	return n & (n - 1) == 0 if n > 0 else False
+def power_of_two_bitstring(n):
+    """only has 1 in binary string."""
+    return n > 0 and bin(n).count('1') == 1
 
-print(power_of_two(7))
+
+def power_of_two_recursive(n):
+    return n == 1 if n < 2 else power_of_two_recursive(n/2)
+
+
+print(power_of_two_recursive(32))
+
 
 def even_number(n):
-	return n & 1 == 0
+    """last bit will be 0."""
+    return n & 1 == 0
+
 
 def odd_number(n):
-	return n & 1 == 1
+    """odd number has 1 in last bit."""
+    return n & 1 == 1
+
 
 print(odd_number(3), even_number(3))
