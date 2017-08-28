38b43ce68b7deda9fd14567d69f52e3f978f682a
Ming Li <ming.george.li@gmail.com>
2017-08-29 00:24:23 +0100

    update fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -27,15 +27,6 @@ def fibonacci2(max):
         a, b = b, a + b
 
 
-def power_of_two(x):
-    return x == 1 if x < 2 else power_of_two(x/2)
-
-
-def log_power_of_two(x):
-    import numpy as np
-    return x > 0 and np.log2(x) % 1 == 0
-
-
 x = 0b1001111
 print(fibonacci1(x))
 print(fibonacci2(x))
