148872badf84370752f45f7512b248eed928453e
Ming Li <ming.george.li@gmail.com>
2017-12-12 13:28:04 +0000

    fetch doc

--- chouse.py
+++ chouse.py
@@ -3,43 +3,77 @@
 """
 chouse
 
-script calling company house API for company profile data.
+script calling UK Companies House API for company profile data.
 A rate limit of 600 requests per 5 minutes imposed.
 """
 
 import os
 import sys
+import base64
+from pprint import pprint
+
 import requests
 
 # pylint: disable=C0111
 
 BASE_URL = "https://api.companieshouse.gov.uk/"
+DOC_URL = "https://document-api.companieshouse.gov.uk/document/{0}/content"
 API_KEY = os.getenv("CHKEY")
+TOKEN = "Basic " + base64.b64encode((API_KEY + ":").encode('ascii')).decode('ascii')
 SESS = requests.Session()
-SESS.auth = (API_KEY, "")
+SESS.headers.update({"Authorization": TOKEN, "Accept": "*/*"})
 VALS = ' '.join(sys.argv[1:])
 
 
 def search_company(query, default_s=SESS, url=BASE_URL+"search/companies"):
     resp = default_s.get(url, params={"q": query, "items_per_page": 5})
-    if resp.status_code == 200:
+    print(resp.request.headers)
+
+    if resp.ok:
         return resp.json()
 
 
-def search_profile(registered_id, default_s=SESS, url=BASE_URL+"/company/{0}"):
+def check_profile(registered_id, default_s=SESS, url=BASE_URL+"/company/{0}"):
     resp = default_s.get(url.format(registered_id))
-    if resp.status_code == 200:
+    print(resp.request.headers)
+
+    if resp.ok:
         return resp.json()
+
+
+def detail_profile(json, key='filing_history', default_s=SESS, url=BASE_URL):
+    try:
+        suburl = json["links"][key]
+    except KeyError:
+        raise Exception("requested detail doesn't exist")
+    resp = default_s.get(url + suburl)
+    print(resp.request.headers)
+
+    if resp.ok:
+        return resp.json()
+
+
+def fetch_document(unique_identifier, default_s=SESS, url=DOC_URL):
+    resp = default_s.get(url.format(unique_identifier))
+    print(resp.request.headers)
+
+    with open('./doc.pdf', 'wb') as filename:
+        filename.write(resp.content)
+    return resp.status_code
 
 
 if __name__ == "__main__":
 
     if not API_KEY:
-        raise Exception("missing Company House API Key")
+        raise Exception("missing Companies House API Key")
     elif not VALS:
         raise Exception("missing search phase or company id.")
 
     if VALS.isdigit():
-        print(search_profile(VALS))
+        base_profile = check_profile(VALS)
+        pprint(base_profile)
+        pprint(detail_profile(base_profile))
+    elif ' ' in VALS or len(VALS) < 20:
+        pprint(search_company(VALS))
     else:
-        print(search_company(VALS))
+        print(fetch_document(VALS))
