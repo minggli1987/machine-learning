5dcd7aec9de7f9675149ff5e5ce7333e0ed373d3
Ming Li <ming.george.li@gmail.com>
2017-12-17 15:01:22 +0000

    revisit

--- gp_deepdive.py
+++ gp_deepdive.py
@@ -7,7 +7,7 @@ from scipy.spatial.distance import cdist, pdist, squareform
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
-
+from sklearn.metrics.pairwise import rbf_kernel
 # np.random.seed(0)
 
 
@@ -35,8 +35,8 @@ def radial_basis_function(Xa, Xb=None, lengthscale=1):
 a = np.random.rand(4, 8)
 b = np.random.normal(size=(100, 8))
 
-# assert np.allclose(radial_basis_function(a, b, lengthscale=1),
-#                    rbf_kernel(a, b, gamma=.5))
+assert np.allclose(radial_basis_function(a, b, lengthscale=1),
+                   rbf_kernel(a, b, gamma=.5))
 
 assert np.allclose(cdist(a, a), squareform(pdist(a)))
 
@@ -60,6 +60,7 @@ K_ss += np.eye(N) * epsilon
 L_ss = np.linalg.cholesky(K_ss)
 # sample from multivariate Gaussian with random white gaussian.
 # TODO shouldn't the gaussian be multivate with 0 mean and np.eye(N) sigma?
+
 f_prior = 0 + np.dot(L_ss, np.random.normal(loc=0, size=(N, 3)))
 
 # f_prior with shape (N, 3)
@@ -88,13 +89,12 @@ def polynomial(X, theta, p=2):
 
 # y_train = polynomial(X_train, theta=[1.2, 2.5], p=1)
 
-
 n = X_train.shape[0]
 K = radial_basis_function(X_train, lengthscale=param)
 K += epsilon * np.eye(n)
 L = np.linalg.cholesky(K)
 
-# conditional probability of multivariate gaussian given f_prior
+# conditional covariance matrix of multivariate gaussian given f_prior
 K_s = radial_basis_function(X_train, X_test, lengthscale=param)
 L_s = np.linalg.solve(L, K_s)
 
@@ -102,27 +102,35 @@ assert np.allclose(np.dot(np.linalg.inv(L), K_s), np.linalg.solve(L, K_s))
 
 
 # TODO !!! why lower triangular matrix L instead of whole kernel matrix?
-# according to Ebden 2008:
+# below derivation explains according to Ebden 2008 Gaussian Processes for Regression:
 # mu = K_s * inv(K) * y
 # = L_s * L * inv(L * L.T) * y
+# using associative property of matrix multiplication
 # = L_s * (L * inv(L)) * inv(L.T) * y
 # = L_s * I * (inv(L.T) * y)
-mean = np.dot(L_s.T, np.dot(np.linalg.inv(L), y_train)).ravel()
+# provable as follows
+assert np.allclose(np.dot(L_s.T, np.dot(np.linalg.inv(L), y_train)),
+                   np.dot(K_s.T, np.dot(np.linalg.inv(K), y_train)))
 
-assert np.allclose(np.dot(np.linalg.inv(L), y_train),
-                   np.linalg.solve(L, y_train))
+# linalg.solve does the same as matrix division
+assert np.allclose(np.dot(np.linalg.inv(L), y_train), np.linalg.solve(L, y_train))
+# thus the black line mean of posterier given y_train
+mu = np.dot(L_s.T, np.linalg.solve(L, y_train)).ravel()
 
 # sample from f ~ posterior given x_test, x_train, y_train points
 # according to Ebden 2008:
 # Σ = K_ss - K_s * inv(K) * K_s.T
 # = K_ss - L_s * L * inv(L * L.T) * (L_s * L).T
 # = K_ss - L_s * L * inv(L) * inv(L.T) * L_s.T * L.T
-# = K_ss - L_s * I * I * L_s.T
+# using associative property of matrix multiplication
+# = K_ss - L_s * (L * inv(L)) * (inv(L.T) * L.T) * L_s.T)
+# = K_ss - L_s * I * I.T * L_s.T
 # = K_ss - L_s * L_s.T
-sigma = K_ss - np.dot(L_s.T, L_s)
-L = np.linalg.cholesky(sigma)
-f_posterior = mean.reshape(-1, 1) + \
-              np.dot(L, np.random.normal(loc=0, size=(N, 3)))
+K_posterior = K_ss - np.dot(L_s.T, L_s)
+L_posterior = np.linalg.cholesky(K_posterior)
+# sampling from posterier multivarate gaussian distribution
+f_posterior = mu.reshape(-1, 1) + \
+              np.dot(L_posterior, np.random.normal(loc=0, size=(N, 3)))
 
 # TODO how to find standard deviation of this posterier?
 var = np.diag(K_ss) - np.sum(L_s**2, axis=0)
@@ -131,11 +139,10 @@ std = np.sqrt(var)
 ax2.plot(X_train, y_train, 'bs', ms=5)
 ax2.plot(X_test, f_posterior)
 # 4 sigma confidence interval roughly 95%
-ax2.fill_between(X_test.ravel(), mean-2*std, mean+2*std, color="#dddddd")
-ax2.plot(X_test, mean, 'r--', lw=2)
+ax2.fill_between(X_test.ravel(), mu-2*std, mu+2*std, color="#dddddd")
+ax2.plot(X_test, mu, 'r--', lw=2)
 ax2.grid(True)
 fig.tight_layout()
-
 
 gp = GaussianProcessRegressor(kernel=RBF(length_scale=param), alpha=epsilon)
 gp.fit(X_train, y_train)
