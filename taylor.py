50014c72bf3551f5325ce623ca0a26966f01ba4c
Ming Li <ming.george.li@gmail.com>
2019-02-18 16:39:33 +0000

    replace \t with

--- taylor.py
+++ taylor.py
@@ -2,16 +2,16 @@ import numpy as np
 
 
 def taylor_exp(x, k=3):
-	approx = 0
+    approx = 0
 
-	for i in range(k):
-		approx += np.power(x, i) / np.math.factorial(i)
+    for i in range(k):
+        approx += np.power(x, i) / np.math.factorial(i)
 
-	return approx
+    return approx
 
 if __name__ == "__main__":
-	val = 3
-	for k in range(19):
-		yhat = taylor_exp(val, k=k)
-		y = np.exp(val)
-		print(f"true exponential function: {y}; taylor approximation at {k}th order: {yhat}")
+    val = 3
+    for k in range(19):
+        yhat = taylor_exp(val, k=k)
+        y = np.exp(val)
+        print(f"true exponential function: {y}; taylor approximation at {k}th order: {yhat}")
