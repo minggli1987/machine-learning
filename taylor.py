7f6ee271169f3ca93e8ddcaa1a43c600f8f954d8
Ming Li <ming.george.li@gmail.com>
2019-02-19 15:56:26 +0000

    add natural log taylor expansion

--- taylor.py
+++ taylor.py
@@ -1,3 +1,17 @@
+"""
+taylor expansion examples
+https://en.wikipedia.org/wiki/Taylor_series
+
+taylor approximation of exponential function
+
+exp(x) ≅ x^0 / 0! + x^1 / 1! + x^2 / 2! + x^3 / 3! + ...
+
+taylor approximation of logarithmic function
+
+log(1 - x) ≅ -x - x^2/2 - x^3/3 - ...
+
+"""
+
 import numpy as np
 
 
@@ -9,9 +23,25 @@ def taylor_exp(x, k=3):
 
     return approx
 
+
+def taylor_log_one_minus(x, k=3):
+	approx = 0
+
+	for i in range(1, k):
+		approx -= np.power(x, i) / i
+
+	return approx
+
 if __name__ == "__main__":
     val = 3
     for k in range(19):
         yhat = taylor_exp(val, k=k)
         y = np.exp(val)
         print(f"true exponential function: {y}; taylor approximation at {k}th order: {yhat}")
+
+    val = .3
+    for k in range(19):
+        yhat = taylor_log_one_minus(val, k=k)
+        y = np.log(1 - val)
+        print(f"true log function: {y}; taylor approximation at {k}th order: {yhat}")
+
