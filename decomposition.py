88f784fc7a6fc115f56ef21e1191b5e1c25f942a
Ming Li <ming.george.li@gmail.com>
2019-09-15 10:50:36 +0000

    relation between singular and eigen values

--- decomposition.py
+++ decomposition.py
@@ -1,8 +1,8 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
 from numpy import cov, dot
-from numpy.linalg import cholesky, eig, svd
+from scipy.linalg import cholesky, eig, svd
 
 m, n = 100, 20
 M = np.random.rand(m, n)
@@ -40,32 +40,53 @@ manual_Sigma = compute_cov(M, rowvar=False)
 assert np.allclose(manual_Sigma, Sigma)
 
 # Cholesky decomposition factorizes a symmetric positive semi-definite matrix A
-# into L so that A = L * L.H, a good way to check singular matrix
-L = cholesky(Sigma)
+# into L so that A = L * L.H, where L is lower triangular matrix
+L = cholesky(Sigma, lower=True)
 assert np.allclose(L @ L.T, Sigma)
-try:
-    cholesky(np.ones((2, 2)))
-except np.linalg.LinAlgError:
-    pass
 
-# Eigen decomposition requires symmetric matrix A so that A.dot(v) = λ * v
+# cholesky is a reasonable way to check singular matrix.
+z = np.random.rand(20, 20)
+phi = z @ z.T
+phi[:, -2:] = 1.0
+with np.testing.assert_raises(np.linalg.LinAlgError):
+    np.linalg.cholesky(phi)
+
+# eigen decomposition requires symmetric matrix A so that A.dot(v) = λ * v
 # where λ is a scalar.
-ev, eig = eig(Sigma)
+evalues, ev = eig(Sigma, right=True)
 # sort eigen values and vectors
-idx = np.argsort(ev)[::-1]
-ev = ev[idx]
-eig = eig[:, idx]
+idx = np.argsort(evalues)[::-1]
+ev = ev[:, idx]
+evalues = evalues[idx]
 
-for i, eigenvector in enumerate(eig.T):
-    assert np.allclose(dot(Sigma, eigenvector), eigenvector * ev[i])
+for i, eigenvector in enumerate(ev.T):
+    assert np.allclose(Sigma @ eigenvector, eigenvector * evalues[i])
 
-assert np.allclose(dot(Sigma, eig), eig * ev)
+# assert eigenvector definition
+assert np.allclose(dot(Sigma, ev), evalues * ev)
 
-U, s, V = svd(Sigma, full_matrices=True)
+U, s, V_h = svd(Sigma, full_matrices=True)
 # S is the singular value matrix, a diagonal matrix same shape as M
 S = np.zeros_like(Sigma)
 S[:n, :n] = np.diag(s)
-assert np.allclose(Sigma, dot(U, dot(S, V)))
+assert np.allclose(Sigma, U @ S @ V_h)
 
 # eigen vectors from eig or svd have occasionally opposite sign
-assert np.allclose(np.abs(U), np.abs(eig))
+assert np.allclose(np.abs(U), np.abs(ev))
+# singular values and eigen values can coincoide for square matrix
+assert np.allclose(s, evalues)
+
+# svd extends to non-symmetric matrix but still requires positive definiteness
+# avoiding the dot product, singular values of M is square root of eigen
+# values from M.T @ M
+U, s, V_h = svd(M, full_matrices=True)
+S = np.zeros_like(M)
+S[:n, :n] = np.diag(s)
+assert np.allclose(M, U @ S @ V_h)
+
+evalues, ev = eig(M.T @ M)
+# requires sorting eigenvectors
+idx = np.argsort(evalues)[::-1]
+evalues = evalues[idx]
+ev = ev[:, idx]
+assert np.allclose(s, np.power(evalues, .5))
